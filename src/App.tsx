8b2ddf51138420497de48777255ee308465d793c
Rodrigo Simao <rodrigofsimao@gmail.com>
2025-08-21 17:16:01 -0300

    modification of lesson 13

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
+import { Heading } from './styles/components/Heading';
+
 import './styles/theme.css';
 import './styles/global.css';
 
 export function App() {
   return (
-    <div>
-      <h1>Olá Mundo</h1>
+    <>
+      <Heading />
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias autem
         deserunt, aperiam obcaecati pariatur cum corrupti quisquam eligendi
         nobis totam, repellat vel officia? Corporis dolorum esse fugiat iusto ea
         architecto!
       </p>
-    </div>
+    </>
   );
 }
